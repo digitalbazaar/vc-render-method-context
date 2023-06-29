23e9db4d839328d5884386428feb14f875bb5933
David I. Lehn <dil@lehn.org>
2023-06-29 18:09:06 -0400

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# @digitalbazaar/FIXME-context ChangeLog
+
+## 1.0.0 - TBD
+
+Initial version.
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+const commonjs = require('rollup-plugin-commonjs');
+
+module.exports = {
+  // Input is created by webpack in previous build step, in CommonJS format.
+  input: 'dist/context.js',
+  output: {
+    file: 'dist/context.esm.js',
+    format: 'esm'
+  },
+  plugins: [
+    commonjs({
+      // explicitly list exports otherwise only have 'default'
+      namedExports: {
+        'dist/context.js': [
+          'CONTEXT',
+          'CONTEXT_URL',
+          'appContextMap',
+          'constants',
+          'contexts'
+        ]
+      }
+    })
+  ]
+};
