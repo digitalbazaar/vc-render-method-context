7c430b7fbab86269f41751f6be140383b9320d94
David I. Lehn <dlehn@digitalbazaar.com>
2023-06-29 20:15:53 -0400

    Change output "context" file to "main".
    This package may have multiple contexts. Using name that does not imply
    only one.

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,16 @@ const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = {
   // Input is created by webpack in previous build step, in CommonJS format.
-  input: 'dist/context.js',
+  input: 'dist/main.js',
   output: {
-    file: 'dist/context.esm.js',
+    file: 'dist/main.esm.js',
     format: 'esm'
   },
   plugins: [
     commonjs({
       // explicitly list exports otherwise only have 'default'
       namedExports: {
-        'dist/context.js': [
+        'dist/main.js': [
           'CONTEXT',
           'CONTEXT_URL',
           'appContextMap',
