8db4137062334c4d41ff50885c420f41f8b456d2
David I. Lehn <dlehn@digitalbazaar.com>
2023-06-29 20:03:20 -0400

    Initial update from template.

--- js/context.js
+++ js/context.js
@@ -1,13 +1,38 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
+/* eslint-disable-next-line quotes */
 'use strict';
 
-module.exports = {
-  '@context': {
-    id: '@id',
-    type: '@type',
-    '@protected': true
+// Use JSON style for context
+/* eslint quotes: ["error", "double"] */
+/* eslint quote-props: ["error", "always"] */
 
+module.exports =
+{
+  "@context": {
+    "@protected": true,
+    "id": "@id",
+    "type": "@type",
+
+    "SvgRenderingTemplate2023": {
+      "@id": "https://w3id.org/vc/render-method#SvgRenderingTemplate2023",
+
+      "@context": {
+        "@protected": true,
+        "id": "@id",
+        "type": "@type",
+
+        "name": "https://schema.org/name",
+        "css3MediaQuery": {
+          "@id": "https://w3id.org/vc/render-method#css3MediaQuery"
+        },
+        "digestMultibase": {
+          "@id": "https://w3id.org/security#digestMultibase",
+          "@type": "https://w3id.org/security#multibase"
+        }
+      }
+    }
   }
-};
+}
+;
